ac99fbf527626df34bffdc3be21780570d825c62
Hunter Krieger <hunterk31@hotmail.com>
2020-04-04 10:49:47 -0400

    first commit

---
+++ ExtendedGame.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Engine
+{
+    class ExtendedGame : Game
+    {
+        protected GraphicsDeviceManager graphics;
+
+        public static ContentManager ContentManager { get; private set; }
+
+        public static int WindowWidth { get; private set; }
+        public static int WindowHeight { get; private set; }
+
+        protected ExtendedGame()
+        {
+            Content.RootDirectory = "Content";
+            graphics = new GraphicsDeviceManager(this);
+
+            WindowWidth = graphics.PreferredBackBufferWidth = 800;
+            WindowHeight = graphics.PreferredBackBufferHeight = 500;
+
+            ContentManager = Content;
+        }
+
+        protected override void LoadContent()
+        {
+            
+        }
+    }
+}
---
+++ GameObject.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Engine
+{
+    class GameObject
+    {
+        protected Texture2D sprite;
+        protected Vector2 velocity, position, origin;
+
+        public GameObject(string this_sprite)
+        {
+            sprite = ExtendedGame.ContentManager.Load<Texture2D>(this_sprite);
+            velocity = Vector2.Zero;
+            position = Vector2.Zero;
+            origin = new Vector2(Width, Height) / 2;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            //position += velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+
+        public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite, position, null, Color.White, 0, origin, 1f, SpriteEffects.None, 0);
+        }
+
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public int Width { get { return sprite.Width; } }
+
+        public int Height { get { return sprite.Height; } }
+
+
+        public Vector2 Velocity
+        {
+            get { return velocity; }
+            set { velocity = value; }
+        }
+
+        public Rectangle BoundingBox
+        {
+            get
+            {
+                // get the sprite's bounds
+                Rectangle spriteBounds = sprite.Bounds;
+                // add the object's position to it as an offset
+                spriteBounds.Offset(position - origin);
+                return spriteBounds;
+            }
+        }
+    }
+}
+
